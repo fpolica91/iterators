00ceca11e6be61de827030620b60d58a05fcf42f
Fabricio <>
2021-04-10 14:06:05 +0000

    create teams.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const myFavouriteAuthors = require('./authors')
+const footballTeams = require('./teams')
 
 for(let author of myFavouriteAuthors){
   console.log('the author name is %s', author)
--- teams.js
+++ teams.js
@@ -1,25 +1,86 @@
 const footballTeams = {
   allTeams: {
     la_liga: [
-      'Barcelona', 
-      'Real Madrid',
-      'Atletico'
+      {name:'Barcelona', points: 65}, 
+      {name:'Real Madrid', points: 63},
+      {name:'Atletico Marid', points: 66}
     ],
     bundesliga: [
-      'Bayern Munchen',
-      'Borussia Dormount',
-      'Borussia Mönchengladbach', 
-      'Union Berlin'
+      {name:'Bayern Munchen', points: 65}, 
+      {name:'Borussia Dormount', points: 63},
+      {name:'Borussia Mönchengladbach', points: 66}
     ],
     serie_a: [
-      'Juventus',
-      'Napoli',
-      'Inter Milan'
+      {name:'Juvents', points: 65}, 
+      {name:'Napoli', points: 63},
+      {name:'Inter Milan', points: 66}
     ],
   },
+   
 
   [Symbol.iterator](){
+    const leagues = Object.values(this.allTeams)
+    let currentLeagueIndex = 0;
+    let currentTeamIndex = 0;
+    return{ 
+      next(){
+        const teamsInCurrentLeague = leagues[currentLeagueIndex]
+        const noMoreTeamsInLeague = !(currentTeamIndex < teamsInCurrentLeague.length)
+        if(noMoreTeamsInLeague){
+          currentLeagueIndex++;
+          currentTeamIndex = 0;
+        }
 
-  }
+        const noMoreLeagues = !(currentLeagueIndex < leagues.length)
 
+        if(noMoreLeagues){
+          return {
+            value: undefined, 
+            done: true
+          }
+        }
+
+        return{
+          value: leagues[currentLeagueIndex][currentTeamIndex++],
+          done: false
+        }
+      }
+    }
+  } 
 }
+
+for(let team of footballTeams){
+  // console.log({
+  //   name: team.name, 
+  //   points: team.points
+  // })
+  console.table(team)
+}
+
+/**
+ * leagues.length is 3
+ * teamsInCurrentLeague.length is the length of each inner array
+ * 
+ */
+ 
+// [
+//   [
+//     { name: 'Barcelona', points: 65 },
+//     { name: 'Real Madrid', points: 63 },
+//     { name: 'Atletico Marid', points: 66 }
+//   ],
+//   [
+//     { name: 'Bayern Munchen', points: 65 },
+//     { name: 'Borussia Dormount', points: 63 },
+//     { name: 'Borussia Mönchengladbach', points: 66 }
+//   ],
+//   [
+//     { name: 'Juvents', points: 65 },
+//     { name: 'Napoli', points: 63 },
+//     { name: 'Inter Milan', points: 66 }
+//   ]
+// ]
+
+
+
+ module.exports = footballTeams
